34d611997e131e7c102fb61c750c40f2e8d19f04
Stephan Griesel <sgriesel@gmail.com>
2018-11-25 18:42:00 +0100

    finish game

--- script.js
+++ script.js
@@ -1,3 +1,157 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
+// unit
+const box = 32;
+
+// images
+const ground = new Image();
+ground.src = "img/bg.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// audio
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
+
+// snake
+let snake = [];
+
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+
+// food
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// score
+let score = 0;
+
+//snake control
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d != "RIGHT"){
+        left.play();
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
+        up.play();
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
+        right.play();
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+        down.play();
+    }
+    if( key == 65 && d != "RIGHT"){
+        left.play();
+        d = "LEFT";
+    }else if(key == 87 && d != "DOWN"){
+        d = "UP";
+        up.play();
+    }else if(key == 68 && d != "LEFT"){
+        d = "RIGHT";
+        right.play();
+    }else if(key == 88 && d != "UP"){
+        d = "DOWN";
+        down.play();
+    }
+}
+
+// collission function
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+// draw to canvas
+function draw(){
+    
+    ctx.drawImage(ground,0,0);
+    
+    for( let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0 )? "#921" : "#654321";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+    
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    // prev head
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // direction
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    
+    // snake eat food then
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        eat.play();
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+        // dont remove tail
+    }else{
+        // remove tail
+        snake.pop();
+    }
+    
+    // add new Head
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    // end
+    
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(game);
+        dead.play();
+    }
+    
+    snake.unshift(newHead);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "45px verdana";
+    ctx.fillText(score,2*box,1.6*box);
+}
+
+function myFunction() {
+    location.reload();
+}
+
+// call function
+
+let game = setInterval(draw,100);
